a898b565700af2f1d23386f7f09afd39129844e7
augjac <71320675+augjac@users.noreply.github.com>
2023-06-13 10:00:13 +0200

    Mise à jour du contenu + SEO + optimisation de la vitesse

--- function.js
+++ function.js
@@ -11,8 +11,24 @@ function toggleMenu() {
       toggle.querySelector("a").innerHTML = "<img src='img/Close.svg' alt='Icone de menu'>";
     }
 }
+// Animation while scrolling
 list.classList.add("pop");
 toggle.addEventListener('click', toggleMenu, false);
 window.addEventListener('scroll', () => {
   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
+//Open tabs
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
